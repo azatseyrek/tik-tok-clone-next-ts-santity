769a0cd71ccd6ed3027285824fc57d279c7428e6
azatseyrek <azatseyrek@hotmail.com>
2022-10-24 16:43:03 +0300

    navbar styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,9 @@
 import type {NextPage} from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
-  const getData = async () => {
-    const resp = await fetch('http://localhost:3000/api/hello');
-    const data = await resp.json();
-    console.log(data);
-  };
-
-  getData()
-
   return (
-    <div className={styles.container}>
-      <h1>Hello tiktak clone!</h1>
+    <div>
+      <h1 className="text-3xl font-bold underline">tiktak clone!</h1>
     </div>
   );
 };
