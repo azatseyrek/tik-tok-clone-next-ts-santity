f8b3128157ec9c187bd7defb4ae7ade48d78b7a7
azatseyrek <azatseyrek@hotmail.com>
2022-10-25 17:59:11 +0300

    topics styling

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -1,12 +1,30 @@
+import Link from 'next/link';
 import React from 'react';
 import { topics } from '../utils/constants';
 
 const Discover = () => {
+  const activeTopicStyle = '';
+
+  const topicStyle =
+    'xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center justify-center cursor-pointer text-black gap-2';
+
   return (
     <div className="xl:border-b-2 xl:border-gray-300 pb-6 ">
       <p className="text-gray-500 font-semibold m-3 mt-4 hidden xl:block">
         Popular Topics
       </p>
+      <div className="flex gap-3 flex-wrap">
+        {topics.map((topic) => (
+          <Link key={topic.name} href={`/?topics=${topic.name}`}>
+            <div className={topicStyle}>
+              <span className="text-2xl xl:text-md ">{topic.icon}</span>
+              <span className="font-md text-md hidden xl:block capitalize">
+                {topic.name}
+              </span>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
