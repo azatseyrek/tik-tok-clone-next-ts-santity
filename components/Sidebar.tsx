fac0e6a1d0dae0dc64b982c7980d127705fcf870
azatseyrek <azatseyrek@hotmail.com>
2022-10-25 16:07:15 +0300

    discover start point

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,5 +1,69 @@
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { useState } from 'react';
+import { GoogleLogin } from 'react-google-login';
+import { AiFillHome, AiOutlineMenu } from 'react-icons/ai';
+import { ImCancelCircle } from 'react-icons/im';
+import Discover from './Discover';
+import SuggestedAccounts from './SuggestedAccounts';
+import Footer from './Footer';
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const [showSidebar, setShowSidebar] = useState(true);
+  const userProfile = false;
+
+  const normalLink =
+    'flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold text-[#f51997] rounded';
+  return (
+    <div className="h-full ">
+      <div
+        className="block xl:hidden m-2 ml-4 mt-3 text-xl cursor-pointer"
+        onClick={() => setShowSidebar((prev) => !prev)}
+      >
+        {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
+      </div>
+      {showSidebar && (
+        <div className="xl:w-400 w-20 flex flex-col justify-start  mb-10 border-r-2 border-gray-300 xl:border-0 p-3">
+          <div className="xl:border-b-2 border-gray-300 xl:pb-4 ">
+            <Link href="/">
+              <div className={normalLink}>
+                <p>{<AiFillHome />}</p>
+                <span className="text-xl hidden xl:block">For you</span>
+              </div>
+            </Link>
+          </div>
+          {!userProfile && (
+            <div className="px-2 py-4 hidden xl:block">
+              <p className="text-gray-400">
+                Log in to like and comment on videos
+              </p>
+              <div className="pr-4">
+                <GoogleLogin
+                  clientId=""
+                  render={(renderProps) => (
+                    <button
+                      className="bg-white text-lg text-[#f51997] border-[1px] border-[#f51997] rounded-md outline-none  px-6 py-3 mt-4 w-full hover:text-white hover:bg-[#f51997] transition-all cursor-pointer hover:shadow-md"
+                      onClick={renderProps.onClick}
+                      disabled={renderProps.disabled}
+                    >
+                      Log in
+                    </button>
+                  )}
+                  onSuccess={() => {}}
+                  onFailure={() => {}}
+                  cookiePolicy={'single_host_origin'}
+                />
+              </div>
+            </div>
+          )}
+          <Discover />
+          <SuggestedAccounts />
+          <Footer />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Sidebar;
